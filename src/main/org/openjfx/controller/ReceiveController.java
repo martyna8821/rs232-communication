50c011982d0a35672bf63613a83a797480ad6a73
tomaszkus97 <tomi88d@interia.pl>
2019-06-27 13:39:35 +0200

    simple ping

--- src/main/org/openjfx/controller/ReceiveController.java
+++ src/main/org/openjfx/controller/ReceiveController.java
@@ -57,7 +57,7 @@ public class ReceiveController implements Initializable {
 
     @FXML
     public void ping(ActionEvent event) {
-        portService.ping();
+        //portService.ping();
     }
 
     @FXML
